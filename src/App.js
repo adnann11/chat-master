17b916442cf686f9288172714b59decfae2d8922
m-triple-m <triplem656@gmail.com>
2021-11-19 11:23:26 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  
-} from "react-router-dom"
-
-import Navbar from './components/Navbar';
-import { Home } from './components/Home';
-import About from './components/About';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import Signup from "./components/signup";
+import Header from "./components/Header";
+import Login from "./components/login";
+import Chat from "./components/chat";
 
 function App() {
   return (
     <>
-    <Router>
-    <Navbar/>
-    
-    <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About/>
-          </Route>
-          <Route exact path="/login">
-            <Login/>
-          </Route>
-          <Route exact path="/signup">
-            <Signup/>
-          </Route>
-        
-        </Switch>
-        </Router>
+      <Router>
+        <Header></Header>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/signup" component={Signup}></Route>
+        <Route path="/chat" component={Chat}></Route>
+      </Router>
     </>
   );
 }
